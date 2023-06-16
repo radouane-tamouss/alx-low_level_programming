9feeace8f414d53da931952f5ee4ccdf9d248d93
radouane-tamouss <atamousse.red@gmail.com>
2023-06-16 03:35:16 -0700

    xD

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,8 +11,11 @@ int main(void)
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 					printf("Last digit of %d is %d ", n, n % 10);
-					if(n % 10 > 5) printf("and is greater than 5\n");
-					if(n % 10 == 0) printf("ans is 0\n")
-					if(n % 10 < 6 && n % 10 != 0) printf("ans is less than 6 and not 0\n")
+					if (n % 10 > 5)
+						printf("and is greater than 5\n");
+					else if (n % 10 == 0)
+						printf("ans is 0\n");
+					else if (n % 10 < 6 && n % 10 != 0)
+						printf("ans is less than 6 and not 0\n");
 					return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.s
@@ -0,0 +1,155 @@
+	.file	"1-last_digit.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Last digit of %d is %d "
+.LC1:
+	.string	"and is greater than 5"
+.LC2:
+	.string	"ans is 0"
+.LC3:
+	.string	"ans is less than 6 and not 0"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, %edi
+	call	time@PLT
+	movl	%eax, %edi
+	call	srand@PLT
+	call	rand@PLT
+	subl	$1073741823, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	cmpl	$5, %edx
+	jle	.L2
+	leaq	.LC1(%rip), %rdi
+	call	puts@PLT
+	jmp	.L3
+.L2:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	testl	%edx, %edx
+	jne	.L4
+	leaq	.LC2(%rip), %rdi
+	call	puts@PLT
+	jmp	.L3
+.L4:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	cmpl	$5, %edx
+	jg	.L3
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	testl	%edx, %edx
+	je	.L3
+	leaq	.LC3(%rip), %rdi
+	call	puts@PLT
+.L3:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
