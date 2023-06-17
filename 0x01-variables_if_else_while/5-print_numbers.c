714358e08d4a1d24457eaf87ab6ff15aa72e5576
radouane-tamouss <atamousse.red@gmail.com>
2023-06-17 10:20:04 -0700

    helo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#inlude<stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+		putchar(i);
+	return (0);
+}
