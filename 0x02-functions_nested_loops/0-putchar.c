1116ecfb1017bd32986cd025269b771fe1f897cd
radouane-tamouss <atamousse.red@gmail.com>
2023-06-20 13:44:16 -0700

    helo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0 ; i < 0 ; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
