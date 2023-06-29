2cbdcef618bd20872884a89bf6080b7bd9214001
radouane-tamouss <atamousse.red@gmail.com>
2023-06-29 13:08:56 -0700

    helo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * take a pointer to an int as parameter
+ * updates the value it point to to 98
+ * Return : (Void)
+ */
+
+void reset_to_98(int *n){
+	*n = 98;
+}
+	
+
